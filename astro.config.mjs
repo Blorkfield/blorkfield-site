639fda63ad33dab890587e909f513198f9fc971b
LittleCarlito <littlecarlitogithub@gmail.com>
2026-01-20 21:19:44 -0600

    fix: passthrough image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import overlayFonts from '@blorkfield/overlay-core/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  image: {
+    service: passthroughImageService()
+  },
   vite: {
     plugins: [overlayFonts()]
   }
